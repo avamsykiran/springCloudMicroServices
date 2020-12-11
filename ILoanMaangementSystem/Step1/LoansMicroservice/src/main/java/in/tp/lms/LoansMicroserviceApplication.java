8e02e605ae06f7610cd85f6ff954fc9f6d13c2a3
Vamsy Kiran Aripaka <a.vamc.it@gmail.com>
2020-12-11 18:07:42 +0530

    step1

--- ILoanMaangementSystem/Step1/LoansMicroservice/src/main/java/in/tp/lms/LoansMicroserviceApplication.java
+++ ILoanMaangementSystem/Step1/LoansMicroservice/src/main/java/in/tp/lms/LoansMicroserviceApplication.java
@@ -2,6 +2,8 @@ package in.tp.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LoansMicroserviceApplication {
@@ -10,4 +12,8 @@ public class LoansMicroserviceApplication {
 		SpringApplication.run(LoansMicroserviceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
 }
